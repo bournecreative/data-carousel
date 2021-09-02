e15fc266e72cd22150b397ba5b47592ae04d65bc
Christian John Gonzalez <bournecreative@gmail.com>
2021-09-02 00:51:26 -0700

    nested an addition template within carousel template to output list of colors to render as circles or available color, clearned up scss, update class names with more meaning names, organized, spacing

--- src/js/components/compare-carousel.js
+++ src/js/components/compare-carousel.js
@@ -2,11 +2,10 @@ import data from "../../data/carouselData.json"
 
 const compareCarousel = {
     init() {
-        const dataCarousel = document.querySelector('.test')
+        const dataCarousel = document.querySelector('.dc__carousel')
         if (!dataCarousel) {
             return
         }
-        console.log(data)
         dataCarousel.innerHTML = this.carouselBuiler(data, this.carouselTemplate)
     },
     carouselBuiler(data, template) {
@@ -18,15 +17,25 @@ const compareCarousel = {
     },
     carouselTemplate(obj) {
         const template = `
-            <div class="content-wrapper">
+            <div class="dc__slide">
                 <h2>${obj.title}</h2>
-                <p>${obj.TakeoffWeight}</p>
-                <p>${obj.MaxDescentSpeed}</p>
-                <p>${obj.MaxFlightTime}</p>
+                <ul class="dc__palette">${generateColors(obj.colors)}</ul>
+                    <p class="dc__line-item">${obj.TakeoffWeight}</p>
+                    <p class="dc__line-item">${obj.MaxDescentSpeed}</p>
+                    <p class="dc__line-item">${obj.MaxFlightTime}</p>
                 <div>
-                <a class="button" href="${obj.ctaUrl}">${obj.cta}</a>
+                <a class="dc__cta" href="${obj.ctaUrl}">${obj.cta}</a>
                 </div>
             </div >`
+
+        function generateColors(arr) {
+            let html = ``
+            arr.map((color) => {
+                html += `<li><span class="${color} dc__palette-color"></span></li>`
+            })
+            return html
+        }
+
         return template;
     }
 
